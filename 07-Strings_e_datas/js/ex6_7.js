57b5c7a0b537d17da83e16e7e6ba1c98fc65bc4a
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-01-04 10:18:08 -0300

    criação do app javascript com exemplo de manipulação de data.

---
+++ 07-Strings_e_datas/js/ex6_7.js
@@ -0,0 +1,4 @@
+let anoAtual = new Date().getFullYear()
+let idade = prompt('Quantos anos você comemora em ' + anoAtual)
+let anoNascimento = anoAtual - idade
+alert('Ah, então você nasceu em ' + anoNascimento)
